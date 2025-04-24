04650c5ecff908f6d3eaa55c9d769c9a5fa17f42
Gabriel BF <gbbolonhez@gmail.com>
2025-04-23 23:22:14 -0300

    test

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom';
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 
+import { Container, TextContent, Title, TitleHighlight } from './styles';
+ 
 const Home = () => {
     return ( <>   
-     <Header></Header>
+     <Header>
+        <Container>
+            <div>
+
+            </div>
+            <div>
+                <img src="" alt="Main image"/>
+            </div>
+        </Container>
+     </Header>
     
 </>
 )
